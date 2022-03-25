b5827ff5dee7dbbf8e10b56b8d861bde90e2a236
hekang42 <hekang@student.42seoul.kr>
2022-03-25 19:23:17 +0900

    Initial Commit

---
+++ kWordle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kWordle
+//
+//  Created by 강희영 on 2022/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kWordle/kWordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  kWordleApp.swift
+//  kWordle
+//
+//  Created by 강희영 on 2022/03/25.
+//
+
+import SwiftUI
+
+@main
+struct kWordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
