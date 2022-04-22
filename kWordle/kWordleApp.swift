d38cd64728f756bbfb1a5e537a384ef28859e8ea
hekang42 <hekang@student.42seoul.kr>
2022-04-22 16:33:32 +0900

    [feat] init Firebase

--- kWordle/kWordleApp.swift
+++ kWordle/kWordleApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct KWordleApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     
     var body: some Scene {
         WindowGroup {
