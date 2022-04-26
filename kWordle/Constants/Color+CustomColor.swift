86ad559f47e6e7e29b804639decc5fcced60dd14
hekang42 <hekang@student.42seoul.kr>
2022-04-26 23:13:03 +0900

    [feat] modify dark Mode

--- kWordle/Constants/Color+CustomColor.swift
+++ kWordle/Constants/Color+CustomColor.swift
@@ -15,4 +15,34 @@ extension Color {
     static let hGray = Color("h_gray")
     static let hLigthGray = Color("h_lightGray")
     static let hWhite = Color("h_white")
+    static let hBlack = Color("h_black")
+}
+
+enum Status: Int {
+    case gray
+    case yellow
+    case green
+    case lightGray
+    case red
+    case white
+    case black
+}
+
+func getColor(of status: Status) -> Color {
+    switch status {
+    case .gray:
+        return Color.hGray
+    case .lightGray:
+        return Color.hLigthGray
+    case .green:
+        return Color.hGreen
+    case .yellow:
+        return Color.hOrange
+    case .red:
+        return Color.hRed
+    case .white:
+        return Color.hWhite
+    case .black:
+        return Color.hBlack
+    }
 }
--- kWordle/View/KeyboardView.swift
+++ kWordle/View/KeyboardView.swift
@@ -24,7 +24,7 @@ struct KeyboardView: View {
                     height: extraButtonSize.height)
                 .foregroundColor(.hLigthGray)
             Text("제출")
-                .foregroundColor(.black)
+                .foregroundColor(getColor(of: .black))
                 .font(.custom("EBSHMJESaeronSB", size: 18))
         }
     }
@@ -36,7 +36,7 @@ struct KeyboardView: View {
                     height: extraButtonSize.height)
                 .foregroundColor(.hLigthGray)
             Text("지움")
-                .foregroundColor(.black)
+                .foregroundColor(getColor(of: .black))
                 .font(.custom("EBSHMJESaeronSB", size: 18))
         }
     }
@@ -101,25 +101,8 @@ struct KeyboardView: View {
                        height: buttonSize.height)
                 .foregroundColor(getColor(of: key.status))
             Text(key.character)
-                .foregroundColor(.black)
+                .foregroundColor(getColor(of: .black))
                 .font(.custom("EBSHMJESaeronR", size: jaum.contains(key.character) ? 24 : 26))
         }
-    }
-}
-
-func getColor(of status: Status) -> Color {
-    switch status {
-    case .gray:
-        return Color.hGray
-    case .lightGray:
-        return Color.hLigthGray
-    case .green:
-        return Color.hGreen
-    case .yellow:
-        return Color.hOrange
-    case .red:
-        return Color.hRed
-    case .white:
-        return Color.white
     }
 }
