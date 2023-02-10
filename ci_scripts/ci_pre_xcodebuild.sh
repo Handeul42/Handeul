8cccdedd1629dcbf4e344cd796644af506a2df75
hekang42 <hekang@student.42seoul.kr>
2023-02-10 17:33:27 +0900

    set up xCodeCloud GoogleService-Info.plist

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,5 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Generating GoogleService-Info.plist from template..."
-cat GoogleService-Info.plist.template | envsubst > ../kWordle/GoogleService-Info.plist
+if [ -z "$GOOGLESERVICE_INFO_PLIST" ]; then
+  echo "Error: Environment variable GOOGLESERVICE_INFO_PLIST is not set."
+  exit 1
+fi
 
+cat > GoogleService-Info.plist << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  $GOOGLESERVICE_INFO_PLIST
+</dict>
+</plist>
+EOF
+
+mv GoogleService-Info.plist ../kWordle/GoogleService-Info.plist 
