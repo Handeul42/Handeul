7b525e41248f0c53b98a9a13c879f328199d41d4
jaemjung <dbdbwer@naver.com>
2023-02-23 15:21:18 +0900

    [fix] Fix life image low resolution issue

--- kWordle/View/LifeView.swift
+++ kWordle/View/LifeView.swift
@@ -27,14 +27,10 @@ struct LifeView: View {
             HStack (spacing: 4) {
                 ForEach(0..<lifeCount, id: \.self) { _ in
                     Image("LifeImage")
-                        .resizable()
-                        .frame(width: 20, height: 30)
                 }
                 if totalLifeCount - lifeCount > 0 {
                     ForEach(0..<totalLifeCount - lifeCount, id: \.self) { _ in
                         Image("emptyLifeImage")
-                            .resizable()
-                            .frame(width: 20, height: 30)
                     }                                 
                 }
             }
@@ -52,8 +48,6 @@ struct LifeView: View {
                     }
                 } label: {
                     Image(systemName: "plus.circle.fill")
-                        .resizable()
-                        .frame(width: 20, height: 20)
                         .foregroundColor(.hBlack)
                 }
                 .padding(.horizontal, 4)
